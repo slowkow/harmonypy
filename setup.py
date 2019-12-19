045f98ac4d5d780a29e7ffba4b4db412cde27725
Kamil Slowikowski <kslowikowski@gmail.com>
2019-12-19 13:03:07 -0500

    change name to harmonypy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="harmonypy-slowkow", # Replace with your own username
+    name="harmonypy",
     version="0.0.1",
     author="Kamil Slowikowski",
     author_email="kslowikowski@gmail.com",

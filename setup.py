76c5748c80a28ea5a29398de03adc4d41f3ef8fe
Kamil Slowikowski <kslowikowski@gmail.com>
2019-12-26 08:49:45 -0600

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name                          = "harmonypy",
-    version                       = "0.0.2",
+    version                       = "0.0.3",
     author                        = "Kamil Slowikowski",
     author_email                  = "kslowikowski@gmail.com",
     description                   = "A data integration algorithm.",

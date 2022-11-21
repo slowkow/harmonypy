9c081b8f12292b67da999e66cd2543e22b8799e3
John Arevalo <johnarevalo@gmail.com>
2022-11-21 16:40:03 +0000

    Use sklearn.cluster.KMeans for init

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['pandas','numpy','scipy'],
+    install_requires=['pandas','numpy','scipy', 'scikit-learn'],
 )
